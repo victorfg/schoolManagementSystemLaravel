bf19aeab377e46ac247619c719868ac7b754acbe
Manuel <m.mendizabal@mail.zeus.vision>
2020-10-11 18:24:19 +0200

    test

--- public/js/app.js
+++ public/js/app.js
@@ -49878,8 +49878,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\xampp\htdocs\schoolManagementSystemLaravel\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\xampp\htdocs\schoolManagementSystemLaravel\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/manuel/studies/p6producto3/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/manuel/studies/p6producto3/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
